82540e53d159782bbb46a693f01a955fa501db3a
George Mendoza <gsmendoza@gmail.com>
2021-07-15 14:31:15 +0800

    Revert "Add Solidus Auth Devise views/controller/routes"
    This reverts commit 94185e9ddafefbb692e8f317a221e23dc047c4ac.

--- lib/solidus_starter_frontend/engine.rb
+++ lib/solidus_starter_frontend/engine.rb
@@ -1,8 +1,7 @@
 # frozen_string_literal: true
 
-require 'solidus_core'
-require 'solidus_support'
-require 'solidus_starter_frontend/authentication_helpers'
+require 'spree/core'
+require 'solidus_starter_frontend'
 
 module SolidusStarterFrontend
   class Engine < Rails::Engine
@@ -16,21 +15,19 @@ module SolidusStarterFrontend
     end
 
     config.to_prepare do
-      # TODO: SolidusAuthDevise was adding these methods to ApplicationController.
-      # We should check if it's really needed and restore this + add this same
-      # include in the generator for who uses the gem copying files into their app.
-      # ApplicationController.include SolidusStarterFrontend::AuthenticationHelpers
-
-      Spree::BaseController.unauthorized_redirect = -> do
-        if try_spree_current_user
-          flash[:error] = I18n.t('spree.authorization_failure')
-
-          redirect_back(fallback_location: spree.unauthorized_path)
-        else
-          store_location
-
-          redirect_back(fallback_location: spree.login_path)
+      if defined?(Spree::Auth::Engine)
+        [
+          Spree::UserConfirmationsController,
+          Spree::UserPasswordsController,
+          Spree::UserRegistrationsController,
+          Spree::UserSessionsController,
+          Spree::UsersController
+        ].each do |auth_controller|
+          auth_controller.include SolidusStarterFrontend::Taxonomies
+          auth_controller.include SolidusStarterFrontend::AuthViews
         end
+
+        Spree::StoreController.include SolidusStarterFrontend::AuthViews
       end
     end
   end
